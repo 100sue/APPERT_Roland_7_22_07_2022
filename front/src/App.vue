f947b0b81271e2719ce31db5656da34c209c3c6d
appert <scoreur@gmail.com>
2022-08-23 15:27:16 +0200

    reprise de projet

---
+++ front/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Signup from "./components/Signup.vue";
+import Login from "./components/Login.vue";
+import News from "./components/News.vue";
+import Edit from "./components/Edit.vue";
+export default {
+  name: "App",
+  components: {
+    
+   Signup,
+    Login,
+    News,
+    Edit,
+    
+  }
+};
+</script>
+<template>
+
+<router-view></router-view> 
+
+</template>
+
+<style>
+</style> 
